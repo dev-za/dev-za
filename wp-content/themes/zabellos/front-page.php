49e19cfbb2ec9335dc826d2bd22fbfa25e845e5f
Valentin Andrushchenko <vandru@ukt.net>
2015-03-26 10:49:58 +0200

    Home page slider

--- wp-content/themes/zabellos/front-page.php
+++ wp-content/themes/zabellos/front-page.php
@@ -9,7 +9,22 @@ get_header();
             <div class="col-xs-12">
                 <div class="row">
                     <div class="home-slaider">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/slide1.jpg" alt="" width="984" height="658" class="img-responsive"/>
+                        <link href="<?php echo get_template_directory_uri(); ?>/css/fotorama/fotorama.css" rel="stylesheet">
+                        <script src="<?php echo get_template_directory_uri(); ?>/js/fotorama.js"></script>
+                        <?php global $postID; $postFields = get_fields($postID);?>
+                        <div class="fotorama" data-width="984"  data-max-width="100%" data-nav="false" data-autoplay="3000">
+                            <?php if($postFields && isset($postFields['images'])){?>
+                                <?php
+
+                                foreach($postFields['images'] as $index=>$image) {?>
+                                    <img src="<?php echo $image['image']['url'];?>" alt="" width="984" class="img-responsive"/>
+                                <?php }
+                                ?>
+                            <?php } ?>
+                        </div>
+<!--                        <img src="--><?php //echo get_template_directory_uri(); ?><!--/images/slide1.jpg" alt="" width="984" height="658" class="img-responsive"/>-->
+
+
                         <div class="home-slider-pagination">
                             <div class="home-paginations-a">
                                 <a class="prev">&lsaquo;</a>
