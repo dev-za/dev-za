a54d0f40da3a16f7987d13897f7152981d1e11fa
Valentin Andrushchenko <vandru@ukt.net>
2015-03-31 11:44:36 +0300

    Gallery Ajax

--- wp-content/themes/zabellos/ajax/load_gallery.php
+++ wp-content/themes/zabellos/ajax/load_gallery.php
@@ -1,18 +1,73 @@
 <?php
 require('../../../../wp-load.php');
-$postID = $_POST['postID'];
-$galleryItems = get_fields($postID);
-sleep(1);
+$itemsPerPage = 6;
+$postID = $_REQUEST['postID'];
+$loadedItems = ($_REQUEST['loadedItems'])?intval($_POST['loadedItems']):0;
+
+
+if($loadedItems)
+    $itemsFrom = $loadedItems;
+else
+    $itemsFrom = 0;
+
+$galleryFields = get_fields($postID);
+$totalItems = count($galleryFields['gallery_items']);
+
+$galleryItems = array_slice($galleryFields['gallery_items'], $itemsFrom, $itemsPerPage);
+//var_dump($galleryFields['gallery_items']);
+//sleep(1);
+$thumbnails = '';
+$popups = '';
+
+foreach($galleryItems as $index => $item) {
+    $thumbnails .= '<div class="col-xs-6 col-sm-4">';
+    $thumbnails .=      '<div class="gallery-img">';
+    $thumbnails .=          '<img src="' . $item['gallery_item']['url'] .'" alt="'.$item['gallery_item']['alt'].'"  width="381" height="382" class="img-responsive" />';
+    $thumbnails .=          '<div class="hover-view">';
+    $thumbnails .=              '<a href="#" role="button" data-toggle="modal" data-target="#gallery-modal-1">View</a>';
+    $thumbnails .=          '</div>';
+    $thumbnails .=      '</div>';
+    $thumbnails .= '</div>';
+
+
+    $popups .= '<div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true" id="gallery-modal-1">';
+    $popups .=      '<div class="modal-dialog">';
+    $popups .=          '<div class="modal-content">';
+    $popups .=                 '<div class="button-close">';
+    $popups .=                      '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>';
+    $popups .=                  '</div>';
+    $popups .=                  '<div class="modal-body">';
+    $popups .=                  '<div class="img-popup">';
+    $popups .=                      '<img src="' . $item['gallery_item']['url'] .'" alt="'.$item['gallery_item']['alt'].'" class="img-responsive" />';
+    $popups .=                   '</div>';
+    $popups .=                   '<div class="popup-comments">';
+
+    $comments = $item['comments_repeater'];
+    foreach($comments as $key => $comment){
+        $popups .=                  '<div class="item-comment '. (($key == 0)?'first-item-comment':'') . '">';
+        $popups .=                    '<p>' . $comment['comment_author'] . '&nbsp';
+        $popups .=                        '<time datetime="'. $comment['comment_date'] .'">' . $comment['comment_date'] .'</time>';
+        $popups .=                     '</p>';
+        $popups .=                    '<p>'. $comment['comment_text'].'</p>';
+        $popups .=                 '</div>';
+
+    }
+
+    $popups .=                    '</div>';
+    $popups .= '</div>';
+    $popups .= '</div>';
+    $popups .= '</div>';
+    $popups .= '</div>';
+
+
+}
+
+$response = new StdClass();
+$response->thumbnails = $thumbnails;
+$response->popups = $popups;
+$response->loadedItems = count($galleryItems);
+$response->totalItems = $totalItems;
+header('Content-Type: application/json');
+echo json_encode($response);
 ?>
-<?php if($galleryItems && isset($galleryItems['gallery_items'])){?>
-    <?php foreach($galleryItems['gallery_items'] as $index=>$image) { ?>
-        <div class="col-xs-6 col-sm-4">
-            <div class="gallery-img">
-                <img src="<?php echo $image['gallery_item']['url'];?>" alt="" width="305" height="305" class="img-responsive" />
-                <div class="hover-view">
-                    <a href="#" role="button" data-toggle="modal" data-target="#gallery-modal-1">View</a>
-                </div>
-            </div>
-        </div>
-    <?php } ?>
-<?php } ?>
+
--- wp-content/themes/zabellos/page-gallery.php
+++ wp-content/themes/zabellos/page-gallery.php
@@ -1,17 +1,39 @@
 <?php get_header(); ?>
 
 <script>
-    $('div#inifiniteLoader').show();
-    $.ajax({
-        url:"/wp-content/themes/zabellos/ajax/load_gallery.php",
-        type: 'POST',
-        data: {postID: '16'},
-        success: function(response) {
-            $('div#inifiniteLoader').hide();
-            $('.gallery-content .container .row').append(response);
-        }
-    });
+    $(document).ready(function($) {
+        window.loadedItems = 0;
+        window.totalItems = 0;
+        loadGalleryItems(window.loadedItems);
+        $(window).scroll(function(){
+            console.log($(document).height());
+            if  ($(window).scrollTop() == $(document).height() - $(window).height()){
+                if(window.loadedItems < window.totalItems){
+                    loadGalleryItems(window.loadedItems);
+                }
 
+            }
+        });
+
+        function loadGalleryItems(loadedItems){
+            $('div#inifiniteLoader').show();
+            $.ajax({
+                url:"/wp-content/themes/zabellos/ajax/load_gallery.php",
+                type: 'GET',
+                data: {postID: '16', loadedItems: loadedItems},
+                success: function(response) {
+                    $('div#inifiniteLoader').hide();
+                    $('.gallery-content .container .row').append(response.thumbnails);
+                    $('.popup').append(response.popups);
+                    if(response.loadedItems)
+                        window.loadedItems = response.loadedItems;
+
+                    window.totalItems = response.totalItems;
+                }
+            });
+
+        }
+    })
 </script>
 
     <?php global $postID; $galleryItems = get_fields($postID);?>
@@ -30,44 +52,7 @@
 <!--    <p class="loading"></p>-->
     <!--popup-content-->
     <div class="popup">
-        <?php if($galleryItems && isset($galleryItems['gallery_items'])){?>
-            <?php foreach($galleryItems['gallery_items'] as $index=>$item) { ?>
 
-                <div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true" id="gallery-modal-1">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="button-close">
-                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="img-popup">
-                                    <img src="<?php echo $item['gallery_item']['url'];?>" alt="" width="305" height="306" class="img-responsive" />
-                                </div>
-                                <div class="popup-comments">
-
-                                    <?php
-                                        $comments = $item['comments_repeater'];
-                                        foreach($comments as $key => $comment){?>
-                                            <div class="item-comment <?php echo ($key == 0)?'first-item-comment':''?>">
-                                                <p><?php echo $comment['comment_author']?>&nbsp;
-                                                    <time datetime="<?php echo $comment['comment_date']?>"><?php echo $comment['comment_date']?></time></p>
-                                                <p><?php echo $comment['comment_text']?></p>
-                                            </div>
-                                    <?php
-                                        }
-                                    ?>
-
-
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            <?php } ?>
-        <?php } ?>
-        <!--#gallery-modal-1-->
     </div>
 
 <?php get_footer()?>
